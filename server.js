606e2d8c3affe26d72e7b2e2bbd6cca3ebacfa8b
KarlenGrigoryan <karlengrigoryan@inbox.ru>
2017-04-19 21:10:00 +0400

    if statement

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var request = require('request');
 var cheerio = require('cheerio');
 var mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost:27017/local');
 mongoose.connect('mongodb://heroku:1234@ds163940.mlab.com:63940/herkoumongodb');
 var Schema = mongoose.Schema;
 
@@ -48,47 +49,65 @@ app.get('/contactlist', function(req, res){
 
         var rowCount = $('.bigborder ').children().length;
 
-        for(var i = 2; i<= rowCount; i++) {
-            place =  $('.bigborder').children().eq(i).children().eq(1).text();
-            jockeycode = $('.bigborder').children().eq(i).children().eq(10).text();
-            trainercode = $('.bigborder').children().eq(i).children().eq(9).text();
-            actualwt = $('.bigborder').children().eq(i).children().eq(13).text();
-            declarhorsewt = $('.bigborder').children().eq(i).children().eq(16).text();
-            lbw = $('.bigborder').children().eq(i).children().eq(11).text();
-            runningposition = $('.bigborder').children().eq(i).children().eq(14).text();
-            finishtime = $('.bigborder').children().eq(i).children().eq(15).text();
-            winodds = $('.bigborder').children().eq(i).children().eq(12).text();
-            user = new User({
-                row:{
-                    td:{
-                        place: place,
-                        jockeycode: jockeycode,
-                        trainercode: trainercode,
-                        actualwt: actualwt,
-                        declarhorsewt: declarhorsewt,
-                        lbw: lbw,
-                        runningposition: runningposition,
-                        finishtime: finishtime,
-                        winodds: winodds
-                    }
-                }
-            });
-            user.save(function (err, user, affected) {
-                if(err) throw  err;
+        User.find(function (err, users) {
+            if(users.length > 0){
+                setTimeout(function() {
+                    User.find(function (err, users) {
+                        //console.log(users);
+                        res.json(users)
+                    })
+                }, 2000);
 
-            });
-        }
+            }else{
+                for(var i = 2; i<= rowCount; i++) {
+                    place =  $('.bigborder').children().eq(i).children().eq(1).text();
+                    jockeycode = $('.bigborder').children().eq(i).children().eq(10).text();
+                    trainercode = $('.bigborder').children().eq(i).children().eq(9).text();
+                    actualwt = $('.bigborder').children().eq(i).children().eq(13).text();
+                    declarhorsewt = $('.bigborder').children().eq(i).children().eq(16).text();
+                    lbw = $('.bigborder').children().eq(i).children().eq(11).text();
+                    runningposition = $('.bigborder').children().eq(i).children().eq(14).text();
+                    finishtime = $('.bigborder').children().eq(i).children().eq(15).text();
+                    winodds = $('.bigborder').children().eq(i).children().eq(12).text();
+                    user = new User({
+                        row:{
+                            td:{
+                                place: place,
+                                jockeycode: jockeycode,
+                                trainercode: trainercode,
+                                actualwt: actualwt,
+                                declarhorsewt: declarhorsewt,
+                                lbw: lbw,
+                                runningposition: runningposition,
+                                finishtime: finishtime,
+                                winodds: winodds
+                            }
+                        }
+                    });
+                    user.save(function (err, user, affected) {
+                        if(err) throw  err;
+
+                    });
+
+                }
+                setTimeout(function() {
+                    User.find(function (err, users) {
+                        //console.log(users);
+                        res.json(users)
+                    })
+                }, 2500);
+            }
+
+        });
+
+
+
         // var page =  $('.bigborder').children().eq(1).text();
 
 
         //console.log(season);
 
-        setTimeout(function() {
-            User.find(function (err, users) {
-                //console.log(users);
-                res.json(users)
-            })
-        }, 6000);
+
 
 
     });
@@ -102,4 +121,4 @@ app.get('/contactlist', function(req, res){
 
 
 app.listen(process.env.PORT || 8000);
-//console.log('sever running on port 8000');
+console.log('sever running on port 8000');
